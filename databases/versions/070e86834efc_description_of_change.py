a9f23c94d6589c102b2d761acdeda05340b05a2a
fultoncarolyn <carolyn.fulton@colorado.edu>
2025-03-17 19:28:30 -0600

    init deployment

---
+++ databases/versions/070e86834efc_description_of_change.py
@@ -0,0 +1,26 @@
+"""[Description of change]
+
+Revision ID: 070e86834efc
+Revises: 
+Create Date: 2025-03-17 19:23:51.048213
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '070e86834efc'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
